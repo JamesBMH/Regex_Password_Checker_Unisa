7b8e9420be0b186e98ea018bc72cbcc9cbca8f28
JamesBMH <jameshertzog3@gmail.com>
2025-02-06 15:28:31 +0200

    Updated readme. Added comments

--- gui.h
+++ gui.h
@@ -4,6 +4,7 @@
 
 #include <QWidget>
 
+//Forward declarations
 class QLineEdit;
 class QCheckBox;
 class QPushButton;
@@ -19,13 +20,22 @@ public:
     ~Gui();
 
 private slots:
+    //Sets up the layout of the GUI
     void setup();
+    //Disables textedit and validate button
     void disableWidgets();
+    //If a checkbox is not clicked the text edit is disabled
+    //If a new checkbox is clicked the text edit is cleared
+    //Sets validator to use depending on selected checkbox
     void checkClicked();
+    //Disables validate button if text edit is empty
     void checkEdit();
+    //Sets validator for text edit
     void setRule1Validator();
     void setRule2Validator();
     void setRule3Validator();
+    //Displays appropriate message after password validation
+    //Clears the text edit if password is wrong and sets focus back on text edit
     void passwordValidation();
 
 private:
